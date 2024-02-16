51fe085a2152a966779ee6667269786e97da680b
Mr. Norkum <kelestayyib@gmail.com>
2024-02-16 11:30:09 +0300

    Successful

--- microshell.c
+++ microshell.c
@@ -9,9 +9,9 @@ int err(const char *s)
 int	cd(char **argv, int i)
 {
 	if (i != 2)
-		return err("error: cd: bad arguments\n");
+		return (err("error: cd: bad arguments\n"));
 	else if (chdir(argv[1]) == -1)
-		return err("error: cd: cannot change directory to "), err(argv[1]), err("\n");
+		return (err("error: cd: cannot change directory to "), err(argv[1]), err("\n"));
 	return (0);
 }
 
@@ -22,20 +22,20 @@ int	exec(char **argv, char **envp, int i)
 	bool	has_pipe = !!argv[i] && !strcmp(argv[i], "|");
 
 	if (has_pipe && pipe(pipefd) == -1)
-		return err("error: fatal\n");
+		return (err("error: fatal\n"));
 
 	int	pid = fork();
 	if (pid == 0)
 	{
 		argv[i] = NULL;
-		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-			return err("error: fatal\n");
+		if (has_pipe && (dup2(pipefd[1], 1) == -1 || close(pipefd[0]) == -1 || close(pipefd[1]) == -1))
+			return (err("error: fatal\n"));
 		execve(*argv, argv, envp);
-		return err("error: cannot execute "), err(*argv), err("\n");
+		return (err("error: cannot execute "), err(*argv), err("\n"));
 	}
 	waitpid(pid, &status, 0);
-	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-		return err("error: fatal\n");
+	if (has_pipe && (dup2(pipefd[0], 0) == -1 || close(pipefd[0]) == -1 || close(pipefd[1]) == -1))
+		return (err("error: fatal\n"));
 	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
 
